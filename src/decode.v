15840dbb44c45e946d48052240a9f13080f34655
Charles Guan <charles.guan@stanford.edu>
2014-02-08 21:45:43 -0800

    alu completed, not yet tested

--- src/decode.v
+++ src/decode.v
@@ -35,7 +35,7 @@ module decode (
     assign rs_addr = instr[25:21];
     assign rt_addr = instr[20:16];
     wire [4:0] rd_addr = instr[15:11];
-    wire [4:0] shamt = instr[10:6];
+    wire [4:0] shamt = isLUI ? 5'd16 : instr[10:6];
     wire [5:0] funct = instr[5:0];
     wire [15:0] immediate = instr[15:0];
 
@@ -44,9 +44,10 @@ module decode (
 //******************************************************************************
 
     // Decides whether the instruction is a taken branch
-    // TODO: Remember that the ALU result is an input to the decode module, so you can
-    // TODO: use the ALU to evaluate the branch condition.
-    // (Should just pass this to the ALU) and read its result
+    // Remember that the ALU result is an input to the decode module, so you can
+    // use the ALU to evaluate the branch condition.
+    wire is_branch_instr = |{op == `BEQ, op == `BNE, op == `BLEZ, op == `BGTZ,
+                             op == `BLTZ_GEZ};
     always @ (*) begin
         case(op)
             `BEQ:  branch_en = rs_data == rt_data;
@@ -75,6 +76,9 @@ module decode (
     assign jump_en = (op == `J) | (op == `JAL);
     assign jump_reg_en = (op == `JR) | (op == `JALR);
 
+    wire jump_no_link = (op == `J) | (op == `JR);
+    wire jump_with_link = (op == `JAL) | (op == `JALR);
+
 //******************************************************************************
 // shift instruction decode
 //******************************************************************************
@@ -85,9 +89,10 @@ module decode (
     wire isSLLV = (op == `SPECIAL) & (funct == `SLLV);
     wire isSRAV = (op == `SPECIAL) & (funct == `SRAV);
     wire isSRLV = (op == `SPECIAL) & (funct == `SRLV);
+    wire isLUI = (op == `LUI);
     
     wire isVarShift = |{isSLLV, isSRAV, isSRLV};
-    wire isShift = |{isSLL, isSRA, isSRL, isVarShift};
+    wire isShift = |{isSLL, isSRA, isSRL, isLUI, isVarShift};
 
 //******************************************************************************
 // ALU instructions decode / control signal for ALU datapath
@@ -128,6 +133,8 @@ module decode (
             {`SW,      `DC6}:  alu_opcode = `ALU_ADD;
             default:           alu_opcode = `ALU_PASSX;
     	endcase
+        if (jump_with_link)
+            alu_opcode = `ALU_ADD;
     end
 
 //******************************************************************************
@@ -136,13 +143,14 @@ module decode (
 
     // zero-extend immediate for bitwise operations
     // for non-bitwise operations, sign-extend or don't care.
+    // See MIPS green sheet for reference.
 
     wire [31:0] imm_sign_extend = {{16{immediate[15]}}, immediate};  
     wire [31:0] imm_zero_extend = {16'b0, immediate};	
 
     reg [31:0] imm_ext;
-    always @* begin
-        if ((op == `ORI) | (op == `ANDI) | (op == `XORI))
+    always @(*) begin
+        if (|{op == `ORI, op == `ANDI, op == `XORI})
             imm_ext = imm_zero_extend;
         else
             imm_ext = imm_sign_extend;
@@ -153,22 +161,21 @@ module decode (
 //******************************************************************************
 
 
-    // TODO: set alu_op_x and alu_op_y based on the kind of operation that
-    // must be performed (and your implementation of the ALU)
-
-    // for shift operations, use either shamt field or lower 5 bits of rs
-    // otherwise use rs
+    // sets alu_op_x and alu_op_y based on the kind of operation that
+    // must be performed
 
     wire [31:0] shift_amount = {27'b0, isVarShift ? rs_data[4:0] : shamt};
-    assign alu_op_x = isShift ? shift_amount : rs_data;
+    assign alu_op_x = jump_with_link ? pc : (isShift ? shift_amount : rs_data);
 
-    wire use_imm_operand = &{op != `SPECIAL, op != `BNE, op != `BEQ};
+    // use immediate operand for I-format instructions
+    // don't use imm_operand for R-format (op == Special)
+    wire use_imm_operand = &{op != `SPECIAL, ~is_branch_instr};
 
-    assign alu_op_y = use_imm_operand ? imm_ext : rt_data;
-    assign reg_write_addr = use_imm_operand ? rt_addr : rd_addr;
+    assign alu_op_y = jump_with_link ? 32'd4 : (use_imm_operand ? imm_ext : rt_data);
+    assign reg_write_addr = jump_with_link ? 5'd31 : (use_imm_operand ? rt_addr : rd_addr); 
     
     // Asserts high when the instruction writes to a register
-    assign reg_write_en = &{op != `SW, op != `J, op != `JR, ~branch_en};
+    assign reg_write_en = &{op != `SW, jump_no_link, ~is_branch_instr};
   
 //******************************************************************************
 // Memory control
